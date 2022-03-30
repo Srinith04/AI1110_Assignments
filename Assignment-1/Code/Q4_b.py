fc523aa8ebb39d7d9ca39582b99408c9bdcfaac6
Srinith04 <102568562+Srinith04@users.noreply.github.com>
2022-03-30 11:52:05 +0530

    Update Q4_b.py

--- Assignment-1/Code/Q4_b.py
+++ Assignment-1/Code/Q4_b.py
@@ -1,22 +1,37 @@
-# Code By Dasari_Srinith
-# Mar 23,2022
-# To check if given two lines are perpendicular
+# Code by Dasari Srinith
+# March 30 ,2022
+# To verify the roots of x^2 +7x -7 = 0 are x=-7.89, x=0.89(rounded to two decimals)
+# by plotting its graph.
 
-# INPUT VALUES
-# lines are 3x-5y-7 =0 and 4x+(12/5)y+9 =0
+import matplotlib.pyplot as plt
+import numpy as np
 
-# If we take the lines of form ax+by+c=0 (i.e slope = -a/b)
-# Line 1
-a1 = 3
-b1 = -5
-Slope1 = 3/5
-# Line 2
-a2 = 4
-b2 = 2.4
-Slope2 = -5/3
+def func(x,a,b,c):
+	return a*x**2+b*x+c
 
-# OUTPUT VALUES
-if Slope1*Slope2==-1:    #Condition for Perpendicular lines
-    print("Yes")
-else:
-    print("No")
+# coeffs are the coefficients of the quadratic equation.
+coeffs = [1,7,-7]
+# Roots are roots of the quadratic equation.
+Roots = np.roots(coeffs)
+Roots = np.round(Roots,2)  # Rounded off to two decimals
+
+x_values = np.linspace(-10,10,1000)
+# Only from -10 to 10 is enough as f(-10) and f(10) are positive
+# and f(0) is negative implying both roots are between -10 and 10
+y_values = func(x_values,1,7,-7)
+
+x_axis = np.zeros(1000)
+plt.plot(x_values,x_axis,color="blue")  # Can be considered as X-Axis
+
+y_axis = np.linspace(min(y_values), max(y_values), 1000)
+plt.plot(x_axis, y_axis,color="blue")   # Can be considered as Y-Axis
+
+plt.plot(x_values, y_values,color="red")   # Graph of the Quadratic equation
+
+points = [*[(a, 0) for a in Roots]]
+for a in points:
+	plt.annotate(str(a), a, xytext=(a[0]+0.1,a[1]+5))
+# So, That the solutions can be seen and verified on the plot
+
+plt.savefig("../Fig/plot.png")
+plt.show()
