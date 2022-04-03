e7fcc404acbb1c07fae8ce69654b2f4e49e5f90e
Srinith04 <102568562+Srinith04@users.noreply.github.com>
2022-04-03 10:27:39 +0530

    Update Q4_c.py

--- Assignment-1/Code/Q4_c.py
+++ Assignment-1/Code/Q4_c.py
@@ -6,9 +6,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def func(x,a,b,c):
-	return a*x**2+b*x+c
-
 # coeffs are the coefficients of the quadratic equation.
 coeffs = [1,7,-7]
 # Roots are roots of the quadratic equation.
@@ -18,7 +15,7 @@ Roots = np.round(Roots,2)  # Rounded off to two decimals
 x_values = np.linspace(-10,10,1000)
 # Only from -10 to 10 is enough as f(-10) and f(10) are positive
 # and f(0) is negative implying both roots are between -10 and 10
-y_values = func(x_values,1,7,-7)
+y_values = x_values**2 + 7*x_values - 7
 
 x_axis = np.zeros(1000)
 plt.plot(x_values,x_axis,color="blue")  # Can be considered as X-Axis
@@ -28,9 +25,11 @@ plt.plot(x_axis, y_axis,color="blue")   # Can be considered as Y-Axis
 
 plt.plot(x_values, y_values,color="red")   # Graph of the Quadratic equation
 
-points = [*[(a, 0) for a in Roots]]
-for a in points:
-	plt.annotate(str(a), a, xytext=(a[0]+0.1,a[1]+5))
+
+plt.plot(Roots[0],0,marker='o',markerfacecolor='Black',markeredgecolor='Black')
+plt.plot(Roots[1],0,marker='o',markerfacecolor='Black',markeredgecolor='Black')
+plt.annotate((Roots[0],0),(Roots[0],0),(Roots[0],4))
+plt.annotate((Roots[1],0),(Roots[1],1),(Roots[1],-8))
 # So, That the solutions can be seen and verified on the plot
 
 plt.savefig("../Fig/plot.png")
