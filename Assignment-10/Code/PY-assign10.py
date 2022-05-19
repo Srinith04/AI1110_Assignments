8f62e9bdec209706bcb9b8d0aefa1fd0f7e3f248
Srinith04 <102568562+Srinith04@users.noreply.github.com>
2022-05-19 21:41:08 +0530

    Update PY-assign10.py

--- Assignment-10/Code/PY-assign10.py
+++ Assignment-10/Code/PY-assign10.py
@@ -1,8 +1,11 @@
 #Code by Dasari Srinith(cs21btech11015)
 
+import numpy as np
+from scipy.stats import binom
+
 n = 4
 p = 1/3
 
 mean = binom.stats(n, p, moments='m')
 
-print("The value of mean is "+ mean)
+print(mean)
