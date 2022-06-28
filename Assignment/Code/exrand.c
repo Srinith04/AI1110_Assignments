afa08c462fbceccd741206ee3ee1229beca9e70a
Srinith04 <102568562+Srinith04@users.noreply.github.com>
2022-06-28 08:10:07 +0530

    Create exrand.c

---
+++ Assignment/Code/exrand.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+    //Uniform random numbers
+    //uniform("uni.dat", 1000000);
+
+    //Gaussian random numbers
+    //gaussian("gau.dat", 1000000);
+
+    //Mean of uniform
+    //printf("%lf\n",mean("uni.dat"));
+
+    //Mean of uniform_sqr
+    //printf("%lf\n",mean_sqr("uni.dat"));
+
+    //Var of uniform
+    //printf("%lf\n",var("uni.dat"));
+
+    //Mean of gau
+    //printf("%lf\n",mean("gau.dat"));
+
+    //Var of gau
+    printf("%lf\n",var("gau.dat"));
+    
+return 0;
+}
+
